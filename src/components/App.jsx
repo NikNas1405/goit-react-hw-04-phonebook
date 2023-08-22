3c562cdd1e3677be0b8754770e1a0a9657c646e9
NikNas1405 <103371039+NikNas1405@users.noreply.github.com>
2023-08-22 22:31:23 +0300

    Code refactoring

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,14 +8,7 @@ import { GlobalStyle } from './GlobalStyle';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-// import initialContacts from './data.json';
-
-const initialContacts = [
-  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-];
+import initialContacts from './data.json';
 
 export default function App() {
   const [contacts, setContacts] = useState(() => {
